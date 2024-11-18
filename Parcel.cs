9ffe29fb3ab864d480c39baedd5a1c70063cf955
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-11-18 12:51:07 +0100

    fixed int -> double

--- Parcel.cs
+++ Parcel.cs
@@ -6,8 +6,8 @@ namespace WebAPIParcelTracking
     {
         [BsonId]
         public string? Id { get; set; }
-        public int? Lat { get; set; }
-        public int? Long { get; set; }
+        public double? Lat { get; set; }
+        public double? Long { get; set; }
         public double? Weight { get; set; }
         public string? Desc { get; set; }
     }
